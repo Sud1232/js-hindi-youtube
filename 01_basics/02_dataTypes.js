b705b0f0709e7ab1ca986bb9d9d35528fc134d76
Sudarshan Patekar <96900392+Sud1232@users.noreply.github.com>
2024-06-26 17:29:25 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert(3+3) //We are using NodeJS not browser
+
+console.log(3+3); //code readability should be high
+console.log("Sudarshan");
+
+
+let name = "John"
+let age = 18
+let isLoggedIn = false
+
+//number=> 2 to power 53
+//Bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => no assigned value
+//symbol=> unique
+
+
+//object
+
+console.log(typeof undefined);
+console.log(typeof null);
