bdacb0f7de0269d13ceb8a0b16f772e72ef29f31
Sudarshan Patekar <96900392+Sud1232@users.noreply.github.com>
2024-07-06 08:55:14 +0000

    more

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,14 +3,56 @@ let a = 300
 if (true) {
     let a = 10
     const b = 20
-    console.log("INNER:",a); 
+    // console.log("INNER:",a);
+
 }
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-    
-}
 
-console.log(a);
+
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    const username = "sudarshan"
+
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "sudarshan"
+    if(username === "sudarshan"){
+        const website = " Youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+} 
+
+// console.log(username);
+
+
+// ++++++++++++++++++++++++ interesting +++++++++++++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num+1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num+2
+}
