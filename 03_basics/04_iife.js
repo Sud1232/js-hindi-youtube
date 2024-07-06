e41e2ebe96aff22f5bca8aadbb2fdc03399ba243
Sudarshan Patekar <96900392+Sud1232@users.noreply.github.com>
2024-07-06 09:47:13 +0000

    IIFE

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,17 @@
+// Immediately Invoked Function Expressions
+
+
+(function chai(){
+    //named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+(  (name)=>{
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('sudarshan')
+
+
+
+
+
+
