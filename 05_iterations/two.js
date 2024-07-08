e6dcc950c3ee5eb5d2377efe80ffdb2cac8f08c3
Sudarshan Patekar <96900392+Sud1232@users.noreply.github.com>
2024-07-08 13:13:04 +0000

    Loops and iterations

---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+
+let index = 0
+// while (index<=10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+// let myArray = ["Spiderman","Wanda","Strange"]
+
+// let arr = 0
+// while(arr < myArray.length){
+//    console.log(`Value is ${myArray[arr]}`);
+//    arr++
+// }
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score +=1
+} while (score<=10);
